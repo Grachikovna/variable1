1644b5f8f04e3dfea06bbfe4b19a623fc2c6b1b7
origin <maryna.oganesyan@yandex.ru>
2024-03-10 16:32:48 +0400

    commit

---
+++ src/Main.java
@@ -0,0 +1,67 @@
+ public class Main {
+     public static void main(String[] args) {
+         //task 1
+         var dog = 8.0;
+         var cat = 3.6;
+         var paper = 763789;
+         System.out.println(dog);
+         System.out.println(cat);
+         System.out.println(paper);
+
+         //task 2
+         dog += 4;
+         cat += 4;
+         paper += 4;
+         System.out.println(dog);
+         System.out.println(cat);
+         System.out.println(paper);
+
+         //task 3
+         dog -= 3.5;
+         cat -= 1.6;
+         paper -= 7639;
+         System.out.println(dog);
+         System.out.println(cat);
+         System.out.println(paper);
+
+         //task 4
+         var friend = 19;
+         System.out.println(friend);
+         friend += 2;
+         System.out.println(friend);
+         friend /= 7;
+         System.out.println(friend);
+
+         //task 5
+         var frog = 3.5;
+         System.out.println(frog);
+         frog *= 10;
+         System.out.println(frog);
+         frog /= 3.5;
+         System.out.println(frog);
+         frog += 4;
+         System.out.println(frog);
+
+         //task 6
+         var boxer1 = 78.2;
+         var boxer2 = 82.7;
+         System.out.println(boxer1 + boxer2);
+         System.out.println(boxer2 - boxer1);
+
+         //task 7
+         System.out.println(boxer2 - boxer1);
+         System.out.println(boxer2 % boxer1);
+
+         //task 8
+         var totalWorkingHours = 640;
+         var workingHours = 8;
+         var employeeCount = totalWorkingHours / workingHours;
+         System.out.println("Всего работников в компании — "+ employeeCount +" человек ");
+
+         employeeCount += 94;
+         totalWorkingHours = employeeCount * workingHours;
+         System.out.println("Если в компании работает " + employeeCount +" человек, то всего "+totalWorkingHours
+                 +" часов работы может быть поделено между сотрудниками ");
+
+     }
+ }
